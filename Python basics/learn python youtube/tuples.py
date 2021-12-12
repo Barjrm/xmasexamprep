a22369292e9d9f2833b4d340ba91bcaf50ce73d3
Baz <you@example.com>
2021-12-12 10:44:10 +0100

    1

---
+++ Python basics/learn python youtube/tuples.py
@@ -0,0 +1,11 @@
+# Tuples imutable = cannot be modified changed added deleted 
+
+coordinates = (1,2)
+
+coordinates[1] = 10 
+
+#tuples cannot be changed...
+#Store data, that never needs to be changed..
+
+# tuples use ()  instead of  [] lists
+
