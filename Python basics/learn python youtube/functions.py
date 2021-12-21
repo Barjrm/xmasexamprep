4e3b66b838c2934741ece7cb37aaff1dc92f9802
Baz <you@example.com>
2021-12-21 08:48:40 +0100

    1

---
+++ Python basics/learn python youtube/functions.py
@@ -0,0 +1,14 @@
+# def say_hi():
+#     user = input("what is your name? ")
+#     print(f"Hello {user}..")
+# say_hi()
+
+
+
+
+def say_hi(user_name, user_age):
+    print(f"Hello {user_name}.. {user_age} years old, wow that's alot")
+user = input("What is your name? ")
+age = input("How old are you? ")
+say_hi(user, age)
+say_hi("Monica",18)
